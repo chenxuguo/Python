b977e2d7ad1ab3108c80699313204e86dab0c561
chenxuguo <806030341@qq.com>
2022-08-25 12:49:44 +0800

    before update home.html to use template base.html

--- myproject/myproject/boards/tests.py
+++ myproject/myproject/boards/tests.py
@@ -8,15 +8,23 @@ from .views import board_topics
 
 # Create your tests here.
 class HomeTests(TestCase):
-    def test_home_view_status_code(self):
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='Django board.')
         url = reverse('home')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.response = self.client.get(url)
+
+    def test_home_view_status_code(self):
+        #        url = reverse('home')
+        #       response = self.client.get(url)
+        self.assertEquals(self.response.status_code, 200)
 
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.func, home)
 
+    def test_home_view_contains_link_to_topic_page(self):
+        board_topic_url = reverse('board_topics', kwargs={'pk': self.board.pk})
+        self.assertContains(self.response, 'href="{0}"'.format(board_topic_url))
 
 class BoardTopicsTests(TestCase):
     def setUp(self):
@@ -35,3 +43,9 @@ class BoardTopicsTests(TestCase):
     def test_board_topic_url_resolves_board_topics_view(self):
         view = resolve('/boards/1/')
         self.assertEquals(view.func, board_topics)
+
+    def test_board_topic_view_contains_link_back_to_homepage(self):
+        board_topic_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(board_topics_url)
+        homepage_url = reverse('home')
+        self.assertContains(response, 'href="{0}"'.format(homepage_url))
