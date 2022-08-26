3d0d0a9f5e3ec2c5461566bbf730395347603962
chenxuguo <806030341@qq.com>
2022-08-26 11:23:07 +0800

    backup views.py before use modelform

--- myproject/myproject/boards/views.py
+++ myproject/myproject/boards/views.py
@@ -1,10 +1,11 @@
 # from django.shortcuts import render
+from django.contrib.auth.models import User
 from django.http import HttpResponse, Http404
 from boards.models import Board
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
+from boards.models import Post, Topic
 
 
 def home(request):
@@ -13,9 +14,36 @@ def home(request):
 
 
 def board_topics(request, pk):
-#    try:
-#        board = Board.objects.get(pk=pk)
-#    except Board.DoesNotExist:
-#        raise Http404
+    #    try:
+    #        board = Board.objects.get(pk=pk)
+    #    except Board.DoesNotExist:
+    #        raise Http404
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
+
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()   # TODO : get the currently logged in user
+    if request.method == 'POST':
+
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        user = User.objects.first()   # TODO: get the currently logged in user
+
+        topic = Topic.objects.create(
+            subject=subject,
+            board=board,
+            starter=user
+        )
+
+        post = Post.objects.create(
+            message=message,
+            topic=topic,
+            created_by=user
+        )
+
+        return redirect('board_topics', pk=board.pk)   # TODO: redirect to the created topic page
+
+    return render(request, 'new_topic.html', {'board': board})
