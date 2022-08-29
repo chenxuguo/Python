39d1eb7a945095e63e10571391353b939fc7a45d
chenxuguo <806030341@qq.com>
2022-08-29 13:35:09 +0800

    1.get login.html backgound picture  2.create templatetags inputclass

--- myproject/myproject/accounts/views.py
+++ myproject/myproject/accounts/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django.shortcuts import redirect
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
 
 
 def signup(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
--- myproject/myproject/myproject/settings.py
+++ myproject/myproject/myproject/settings.py
@@ -129,3 +129,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+LOGOUT_REDIRECT_URL = 'home'
+
+LOGIN_REDIRECT_URL = 'home'
+
